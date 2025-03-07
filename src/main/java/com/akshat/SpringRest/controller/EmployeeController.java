a1eb95d9f8c5e571205b63e2617af0d6be3bcb1e
akshat-ttn <akshat.srivastava@tothenew.com>
2025-03-07 17:29:24 +0530

    Add Swagger Description

--- src/main/java/com/akshat/SpringRest/controller/EmployeeController.java
+++ src/main/java/com/akshat/SpringRest/controller/EmployeeController.java
@@ -17,32 +17,31 @@ public class EmployeeController {
     @Autowired
     private EmployeeService service;
 
+    @Operation(summary = "Get all employees", description = "Fetches a list of all employees")
     @GetMapping("")
     public ResponseEntity<List<Employee>> getAllEmployees() {
         return service.getAllEmployees();
     }
 
-//    @Operation(summary = "getUserById", description = "Returns a user as per the id")
+    @Operation(summary = "Get employee by ID", description = "Fetches a single employee by their ID")
     @GetMapping("/{id}")
     public ResponseEntity<Employee> getEmployeeById(@PathVariable Integer id) {
         return service.getEmployeeById(id);
     }
 
+    @Operation(summary = "Create a new employee", description = "Creates a new employee with the given details")
     @PostMapping("/create")
     public ResponseEntity<Employee> createEmployee(@Valid @RequestBody Employee employee) {
         return service.createEmployee(employee);
     }
 
-//        @PostMapping(path = "/create", consumes = MediaType.APPLICATION_XML_VALUE)
-//        public ResponseEntity<Employee> createEmployee(@Valid @RequestBody Employee employee) {
-//            return service.createEmployee(employee);
-//        }
-
+    @Operation(summary = "Update an employee", description = "Updates an existing employee by their ID")
     @PutMapping("/update/{id}")
     public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee, @PathVariable Integer id) {
         return service.updateEmployee(employee, id);
     }
 
+    @Operation(summary = "Delete an employee", description = "Deletes an employee by their ID")
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> deleteEmployeeById(@PathVariable Integer id) {
         return service.deleteEmployeeById(id);
@@ -52,5 +51,8 @@ public class EmployeeController {
 }
 
 
-
+//        @PostMapping(path = "/create", consumes = MediaType.APPLICATION_XML_VALUE)
+//        public ResponseEntity<Employee> createEmployee(@Valid @RequestBody Employee employee) {
+//            return service.createEmployee(employee);
+//        }
 
